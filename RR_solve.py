334c91a3b8a000028aa9e3e51f7e09bd451bc0e6
Varun-1712 <varunthummar789@outlook.com>
2022-04-26 09:24:46 +0530

    second commit

---
+++ RR_solve.py
@@ -0,0 +1,19 @@
+def exuction_for_RR(process):
+    # Sort the process according to their arrival time
+    process.sort(key=lambda x: x["arrival_time"])
+    queue_list = []
+    
+    time_count=process[0]["arrival_time"];
+    for i in range(0,process.length())
+        if process[i]["arrical_time"]<=time_count:
+            queue_list.append(i)
+    i=0
+    while complete_process != process.length():
+        if process[i]["remaining_burst_time"] == 0:
+            continue
+
+        
+          
+
+
+          
